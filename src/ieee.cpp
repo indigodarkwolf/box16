f6aa8b309407480e1b803577fde4f3370f305d65
Stephen Horn <indigo.darkwolf@gmail.com>
2023-06-25 13:18:43 -0500

    Fix crash in hostfs enumerating subdirectories in Linux

--- src/ieee.cpp
+++ src/ieee.cpp
@@ -409,7 +409,7 @@ static int continue_directory_listing(uint8_t *data)
 			}
 		}
 
-		int file_size = static_cast<int>((std::filesystem::file_size(dp.path()) + 255) / 256);
+		int file_size = std::filesystem::is_directory(st) ? 0 : static_cast<int>((std::filesystem::file_size(dp.path()) + 255) / 256);
 		if (file_size > 0xFFFF) {
 			file_size = 0xFFFF;
 		}
