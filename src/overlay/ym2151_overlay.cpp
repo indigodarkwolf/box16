82432e9f16091ae77a1bff617c8a2d222c88e9c3
mooinglemur <mooinglemur@users.noreply.github.com>
2025-02-07 21:00:38 -0700

    sprintf cleanups

--- src/overlay/ym2151_overlay.cpp
+++ src/overlay/ym2151_overlay.cpp
@@ -128,7 +128,7 @@ void draw_debugger_ym2151()
 				}
 				ImGui::TableNextColumn();
 				char buf[5];
-				std::sprintf(buf, "%d", tim->cur);
+				std::snprintf(buf, 5, "%d", tim->cur);
 				ImGui::ProgressBar(tim->cur / (float)TIM_MAX, ImVec2(0, 0), buf);
 				ImGui::SameLine();
 				ImGui::Text("Counter");
@@ -219,7 +219,7 @@ void debugger_draw_ym_lfo_and_noise(uint8_t *regs)
 		}
 		ImGui::TableNextColumn();
 		char buf[4];
-		std::sprintf(buf, "%d", (int)(lcnt * 256));
+		std::snprintf(buf, 4, "%d", (int)(lcnt * 256));
 		ImGui::ProgressBar(lcnt, ImVec2(0, 0), buf);
 		ImGui::SameLine(0, ImGui::GetStyle().ItemInnerSpacing.x);
 		ImGui::Text("Cur. Phase");
@@ -394,7 +394,7 @@ void debugger_draw_ym_voice(int i, uint8_t *regs, ym_channel_data &ch, ym_keyon_
 		const uint8_t oct  = note / 12;
 		// C#8 +00.0
 		char kcinfo[12];
-		std::sprintf(kcinfo, "%c%c%d %+05.1f", notes[ni], notes[ni + 1], oct, cents);
+		std::snprintf(kcinfo, 12, "%c%c%d %+05.1f", notes[ni], notes[ni + 1], oct, cents);
 		ImGui::SetNextItemWidth(-28);
 		if (ImGui::SliderFloat("KC", &ch.kc, 0, 96, kcinfo, ImGuiSliderFlags_NoRoundToFormat)) {
 			fpkc = std::min((int)(ch.kc * 256), (96 * 256) - 1);
@@ -563,7 +563,7 @@ void debugger_draw_ym_voice(int i, uint8_t *regs, ym_channel_data &ch, ym_keyon_
 			ImGui::TableNextColumn();
 			char buf[11] = ".5";
 			if (slot.mul > 0) {
-				std::sprintf(buf, "%d", slot.mul);
+				std::snprintf(buf, 11, "%d", slot.mul);
 			}
 			ImGui::SetNextItemWidth(-FLT_MIN);
 			if (ImGui::SliderInt("mul", &slot.mul, 0, 15, buf)) {
@@ -645,7 +645,7 @@ void debugger_draw_ym_voice(int i, uint8_t *regs, ym_channel_data &ch, ym_keyon_
 			ImGui::PopID();
 			float out = slot_state.final_env;
 			char  buf2[5];
-			std::sprintf(buf2, "%d", (int)((1 - out) * 1024));
+			std::snprintf(buf2, 5, "%d", (int)((1 - out) * 1024));
 			ImGui::TableNextColumn();
 			ImGui::ProgressBar(out, ImVec2(-FLT_MIN, 0), buf2);
 			if (ImGui::IsItemHovered()) {
