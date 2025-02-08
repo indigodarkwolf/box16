c4024700075c6dfdc314fd820b48242969a4f0ee
mooinglemur <mooinglemur@users.noreply.github.com>
2025-02-07 20:50:05 -0700

    C++ convention?

--- src/files.cpp
+++ src/files.cpp
@@ -8,7 +8,7 @@
 #include <unistd.h> // Added to resolve Microsoft c++ warnings around POSIX and other depreciated errors.
 #include <zlib.h>
 
-#include <sstream.h>
+#include <sstream>
 
 #include "options.h"
 #include "zlib.h"
